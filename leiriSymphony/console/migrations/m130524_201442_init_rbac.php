acccd968993b4b67c5c951cf3f08bd32f922ed60
Bernardo Santos <2190723@my.ipleiria.pt>
2021-12-03 23:43:40 +0000

    prems and roles updated

--- leiriSymphony/console/migrations/m130524_201442_init_rbac.php
+++ leiriSymphony/console/migrations/m130524_201442_init_rbac.php
@@ -8,6 +8,42 @@ class m130524_201442_init_rbac extends Migration
     {
         $auth = Yii::$app->authManager;
 
+        //utilizadores
+        $criarUtilizador = $auth->createPermission('criarUtilizador');
+        $criarUtilizador->description = 'Criar um Utilizador';
+        $auth->add($criarUtilizador);
+
+        $verUtilizador = $auth->createPermission('verUtilizador');
+        $verUtilizador->description = 'Ver um Utilizador';
+        $auth->add($verUtilizador);
+
+        $editarUtilizador = $auth->createPermission('editarUtilizador');
+        $editarUtilizador->description = 'Editar um Utilizador';
+        $auth->add($editarUtilizador);
+
+        $eliminarUtilizador = $auth->createPermission('eliminarUtilizador');
+        $eliminarUtilizador->description = 'Eliminar um Utilizador';
+        $auth->add($eliminarUtilizador);
+
+
+        //perfis
+        $criarPerfil = $auth->createPermission('criarPerfil');
+        $criarPerfil->description = 'Criar um Perfil';
+        $auth->add($criarPerfil);
+
+        $verPerfil = $auth->createPermission('verPerfil');
+        $verPerfil->description = 'Ver um Perfil';
+        $auth->add($verPerfil);
+
+        $editarPerfil = $auth->createPermission('editarPerfil');
+        $editarPerfil->description = 'Editar um Perfil';
+        $auth->add($editarPerfil);
+
+        $eliminarPerfil = $auth->createPermission('eliminarPerfil');
+        $eliminarPerfil->description = 'Eliminar um Perfil';
+        $auth->add($eliminarPerfil);
+
+
         //produtos
         $criarProduto = $auth->createPermission('criarProduto');
         $criarProduto->description = 'Criar um produto';
@@ -44,6 +80,24 @@ class m130524_201442_init_rbac extends Migration
         $auth->add($eliminarCategoria);
 
 
+        //subcategorias
+        $criarSubCategoria = $auth->createPermission('criarSubCategoria');
+        $criarSubCategoria->description = 'Criar uma Subcategoria';
+        $auth->add($criarSubCategoria);
+
+        $verSubCategoria = $auth->createPermission('verSubCategoria');
+        $verSubCategoria->description = 'Ver uma Subcategoria';
+        $auth->add($verSubCategoria);
+
+        $editarSubCategoria = $auth->createPermission('editarSubCategoria');
+        $editarSubCategoria->description = 'Editar uma Subcategoria';
+        $auth->add($editarSubCategoria);
+
+        $eliminarSubCategoria = $auth->createPermission('eliminarSubCategoria');
+        $eliminarSubCategoria->description = 'Eliminar uma Subcategoria';
+        $auth->add($eliminarSubCategoria);
+
+
         //marcas
         $criarMarca = $auth->createPermission('criarMarca');
         $criarMarca->description = 'Criar uma marca';
@@ -60,6 +114,42 @@ class m130524_201442_init_rbac extends Migration
         $eliminarMarca = $auth->createPermission('eliminarMarca');
         $eliminarMarca->description = 'Eliminar uma marca';
         $auth->add($eliminarMarca);
+
+
+        //encomendas
+        $criarEncomenda = $auth->createPermission('criarEncomenda');
+        $criarEncomenda->description = 'Criar uma Encomenda';
+        $auth->add($criarEncomenda);
+
+        $verEncomenda = $auth->createPermission('verEncomenda');
+        $verEncomenda->description = 'Ver uma Encomenda';
+        $auth->add($verEncomenda);
+
+        $editarEncomenda = $auth->createPermission('editarEncomenda');
+        $editarEncomenda->description = 'Editar uma Encomenda';
+        $auth->add($editarEncomenda);
+
+        $eliminarEncomenda = $auth->createPermission('eliminarEncomenda');
+        $eliminarEncomenda->description = 'Eliminar uma Encomenda';
+        $auth->add($eliminarEncomenda);
+
+
+        //artistas
+        $criarArtista = $auth->createPermission('criarArtista');
+        $criarArtista->description = 'Criar um Artista';
+        $auth->add($criarArtista);
+
+        $verArtista = $auth->createPermission('verArtista');
+        $verArtista->description = 'Ver um Artista';
+        $auth->add($verArtista);
+
+        $editarArtista = $auth->createPermission('editarArtista');
+        $editarArtista->description = 'Editar um Artista';
+        $auth->add($editarArtista);
+
+        $eliminarArtista = $auth->createPermission('eliminarArtista');
+        $eliminarArtista->description = 'Eliminar um Artista';
+        $auth->add($eliminarArtista);
 
 
         //albuns
@@ -116,22 +206,40 @@ class m130524_201442_init_rbac extends Migration
         $auth->add($eliminarEvento);
 
 
-        //encomendas
-        $criarEncomenda = $auth->createPermission('criarEncomenda');
-        $criarEncomenda->description = 'Criar uma Encomenda';
-        $auth->add($criarEncomenda);
+        //demonstrações
+        $criarDemonstracao = $auth->createPermission('criarDemonstracao');
+        $criarDemonstracao->description = 'Criar uma Demonstração';
+        $auth->add($criarDemonstracao);
 
-        $verEncomenda = $auth->createPermission('verEncomenda');
-        $verEncomenda->description = 'Ver uma Encomenda';
-        $auth->add($verEncomenda);
+        $verDemonstracao = $auth->createPermission('verDemonstracao');
+        $verDemonstracao->description = 'Ver uma Demonstração';
+        $auth->add($verDemonstracao);
 
-        $editarEncomenda = $auth->createPermission('editarEncomenda');
-        $editarEncomenda->description = 'Editar uma Encomenda';
-        $auth->add($editarEncomenda);
+        $editarDemonstracao = $auth->createPermission('editarDemonstracao');
+        $editarDemonstracao->description = 'Editar uma Demonstração';
+        $auth->add($editarDemonstracao);
 
-        $eliminarEncomenda = $auth->createPermission('eliminarEncomenda');
-        $eliminarEncomenda->description = 'Eliminar uma Encomenda';
-        $auth->add($eliminarEncomenda);
+        $eliminarDemonstracao = $auth->createPermission('eliminarDemonstracao');
+        $eliminarDemonstracao->description = 'Eliminar uma Demonstração';
+        $auth->add($eliminarDemonstracao);
+
+
+        //tipoinformações
+        $criarTipoInformacao = $auth->createPermission('criarTipoInformacao');
+        $criarTipoInformacao->description = 'Criar uma TipoInformacao';
+        $auth->add($criarTipoInformacao);
+
+        $verTipoInformacao = $auth->createPermission('verTipoInformacao');
+        $verTipoInformacao->description = 'Ver uma TipoInformacao';
+        $auth->add($verTipoInformacao);
+
+        $editarTipoInformacao = $auth->createPermission('editarTipoInformacao');
+        $editarTipoInformacao->description = 'Editar uma TipoInformacao';
+        $auth->add($editarTipoInformacao);
+
+        $eliminarTipoInformacao = $auth->createPermission('eliminarTipoInformacao');
+        $eliminarTipoInformacao->description = 'Eliminar uma TipoInformacao';
+        $auth->add($eliminarTipoInformacao);
 
 
             //Roles
@@ -139,6 +247,14 @@ class m130524_201442_init_rbac extends Migration
         $admin = $auth->createRole('Administrador');
         $auth->add($admin);
 
+        $auth->addChild($admin, $criarUtilizador);
+        $auth->addChild($admin, $verUtilizador);
+        $auth->addChild($admin, $editarUtilizador);
+        $auth->addChild($admin, $eliminarUtilizador);
+        $auth->addChild($admin, $criarPerfil);
+        $auth->addChild($admin, $verPerfil);
+        $auth->addChild($admin, $editarPerfil);
+        $auth->addChild($admin, $eliminarPerfil);
         $auth->addChild($admin, $criarProduto);
         $auth->addChild($admin, $verProduto);
         $auth->addChild($admin, $editarProduto);
@@ -147,10 +263,22 @@ class m130524_201442_init_rbac extends Migration
         $auth->addChild($admin, $verCategoria);
         $auth->addChild($admin, $editarCategoria);
         $auth->addChild($admin, $eliminarCategoria);
+        $auth->addChild($admin, $criarSubCategoria);
+        $auth->addChild($admin, $verSubCategoria);
+        $auth->addChild($admin, $editarSubCategoria);
+        $auth->addChild($admin, $eliminarSubCategoria);
         $auth->addChild($admin, $criarMarca);
         $auth->addChild($admin, $verMarca);
         $auth->addChild($admin, $editarMarca);
         $auth->addChild($admin, $eliminarMarca);
+        $auth->addChild($admin, $criarEncomenda);
+        $auth->addChild($admin, $verEncomenda);
+        $auth->addChild($admin, $editarEncomenda);
+        $auth->addChild($admin, $eliminarEncomenda);
+        $auth->addChild($admin, $criarArtista);
+        $auth->addChild($admin, $verArtista);
+        $auth->addChild($admin, $editarArtista);
+        $auth->addChild($admin, $eliminarArtista);
         $auth->addChild($admin, $criarAlbum);
         $auth->addChild($admin, $verAlbum);
         $auth->addChild($admin, $editarAlbum);
@@ -163,10 +291,15 @@ class m130524_201442_init_rbac extends Migration
         $auth->addChild($admin, $verEvento);
         $auth->addChild($admin, $editarEvento);
         $auth->addChild($admin, $eliminarEvento);
-        $auth->addChild($admin, $criarEncomenda);
-        $auth->addChild($admin, $verEncomenda);
-        $auth->addChild($admin, $editarEncomenda);
-        $auth->addChild($admin, $eliminarEncomenda);
+        $auth->addChild($admin, $criarDemonstracao);
+        $auth->addChild($admin, $verDemonstracao);
+        $auth->addChild($admin, $editarDemonstracao);
+        $auth->addChild($admin, $eliminarDemonstracao);
+        $auth->addChild($admin, $criarTipoInformacao);
+        $auth->addChild($admin, $verTipoInformacao);
+        $auth->addChild($admin, $editarTipoInformacao);
+        $auth->addChild($admin, $eliminarTipoInformacao);
+        
 
         //gestor de loja
         $gestor = $auth->createRole('Gestor de loja');
@@ -180,10 +313,18 @@ class m130524_201442_init_rbac extends Migration
         $auth->addChild($gestor, $verCategoria);
         $auth->addChild($gestor, $editarCategoria);
         $auth->addChild($gestor, $eliminarCategoria);
+        $auth->addChild($gestor, $criarSubCategoria);
+        $auth->addChild($gestor, $verSubCategoria);
+        $auth->addChild($gestor, $editarSubCategoria);
+        $auth->addChild($gestor, $eliminarSubCategoria);
         $auth->addChild($gestor, $criarMarca);
         $auth->addChild($gestor, $verMarca);
         $auth->addChild($gestor, $editarMarca);
         $auth->addChild($gestor, $eliminarMarca);
+        $auth->addChild($gestor, $criarArtista);
+        $auth->addChild($gestor, $verArtista);
+        $auth->addChild($gestor, $editarArtista);
+        $auth->addChild($gestor, $eliminarArtista);
         $auth->addChild($gestor, $criarAlbum);
         $auth->addChild($gestor, $verAlbum);
         $auth->addChild($gestor, $editarAlbum);
@@ -195,32 +336,43 @@ class m130524_201442_init_rbac extends Migration
         $auth->addChild($gestor, $editarEncomenda);
         $auth->addChild($gestor, $verEncomenda);
 
+
         //apoio ao cliente
         $apoio = $auth->createRole('Apoio ao cliente');
         $auth->add($apoio);
 
         $auth->addChild($apoio, $verProduto);
         $auth->addChild($apoio, $verCategoria);
+        $auth->addChild($apoio, $verSubCategoria);
         $auth->addChild($apoio, $verMarca);
+        $auth->addChild($apoio, $verArtista);
         $auth->addChild($apoio, $verAlbum);
         $auth->addChild($apoio, $verMusica);
         $auth->addChild($apoio, $verEvento);
         $auth->addChild($apoio, $editarEncomenda);
         $auth->addChild($apoio, $verEncomenda);
+        $auth->addChild($apoio, $verTipoInformacao);
+        $auth->addChild($apoio, $editarTipoInformacao);
+        
 
         //cliente
         $cliente = $auth->createRole('Cliente');
         $auth->add($cliente);
 
+        $auth->addChild($cliente, $verPerfil);
+        $auth->addChild($cliente, $editarPerfil);
         $auth->addChild($cliente, $verProduto);
         $auth->addChild($cliente, $verCategoria);
+        $auth->addChild($cliente, $verSubCategoria);
         $auth->addChild($cliente, $verMarca);
+        $auth->addChild($cliente, $verArtista);
         $auth->addChild($cliente, $verAlbum);
         $auth->addChild($cliente, $verMusica);
         $auth->addChild($cliente, $verEvento);
-        $auth->addChild($cliente, $criarEncomenda);
+        $auth->addChild($cliente, $verDemonstracao);
+        $auth->addChild($cliente, $editarEncomenda);
         $auth->addChild($cliente, $verEncomenda);
-
+        $auth->addChild($cliente, $criarTipoInformacao);
 
 
         $auth->assign($admin, 1);
