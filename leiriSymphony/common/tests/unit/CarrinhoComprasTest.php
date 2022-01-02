643bf1187c2e47814da688662df4f0f36c273570
joaoantunes76 <2190732@my.ipleiria.pt>
2022-01-02 16:28:22 +0000

    Created CarrinhoCompras Test and configured unit.suite.yml

---
+++ leiriSymphony/common/tests/unit/CarrinhoComprasTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace common\tests;
+
+class CarrinhoComprasTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \common\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
