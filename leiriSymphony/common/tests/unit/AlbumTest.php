0cf4e67cfbccf6ad6a1bf3fb484aeeca7717f360
Bernardo Santos <2190723@my.ipleiria.pt>
2022-01-03 20:43:02 +0000

    AlbumTest completed

--- leiriSymphony/common/tests/unit/AlbumTest.php
+++ leiriSymphony/common/tests/unit/AlbumTest.php
@@ -2,7 +2,9 @@
 namespace common\tests;
 
 use common\models\Albuns;
+use common\models\Albunsartistas;
 use common\models\Artistas;
+use common\models\Musicas;
 
 class AlbumTest extends \Codeception\Test\Unit
 {
@@ -20,6 +22,9 @@ class AlbumTest extends \Codeception\Test\Unit
     }
 
     // tests
+    /*
+     * Albuns
+     */
     public function testAlbumNomeNumeroInteiroNaoValida()
     {
         $album = new Albuns();
@@ -121,11 +126,14 @@ class AlbumTest extends \Codeception\Test\Unit
         $this->assertIsNumeric($album->delete());
     }
 
-    public function testVerSeAlbumFoiApagado()
+    public function testVerificarSeAlbumFoiApagado()
     {
         $this->tester->dontSeeInDatabase(Albuns::tableName(), ['nome' => 'O monstro precisa de amigos', 'preco' => 11]);
     }
 
+    /*
+     * Artistas
+     */
     public function testArtistaNomeNumeroInteiroNaoValida()
     {
         $artista = new Artistas();
@@ -159,6 +167,187 @@ class AlbumTest extends \Codeception\Test\Unit
         $this->tester->seeInDatabase(Artistas::tableName(), ['nome' => 'Rui Veloso']);
     }
 
-    
+    public function testAlterarArtistaRegistado()
+    {
+        $artista = Artistas::find()->where(['nome' => 'Rui Veloso'])->one();
+        $artista->nome = 'João Pedro Pais';
 
+        $this->assertTrue($artista->save());
+    }
+
+    public function testVerificarArtistaAlterado()
+    {
+        $this->tester->seeInDatabase(Artistas::tableName(), ['nome' => 'João Pedro Pais']);
+    }
+
+    public function testEliminarArtista()
+    {
+        $artista = Artistas::find()->where(['nome' => 'João Pedro Pais'])->one();
+        $this->assertIsNumeric($artista->delete());
+    }
+
+    public function testVerificarSeArtistaFoiApagado()
+    {
+        $this->tester->dontSeeInDatabase(Artistas::tableName(), ['nome' => 'João Pedro Pais']);
+    }
+
+    /*
+     * Albunsartistas
+     */
+    public function testRelacaoAlbunsartistasIdAlbumInexistenteNaoValida()
+    {
+        $albumartista = new Albunsartistas();
+        $albumartista->idalbum = 123124; //album inexistente
+        $albumartista->idartista = 3;
+
+        $this->assertFalse($albumartista->validate());
+    }
+
+    public function testRelacaoAlbunsartistasIdAlbumNuloNaoValida()
+    {
+        $albumartista = new Albunsartistas();
+        $albumartista->idalbum = null; //album não pode ser nulo
+        $albumartista->idartista = 3;
+
+        $this->assertFalse($albumartista->validate());
+    }
+
+    public function testRelacaoAlbunsartistasIdArtistaInexistenteNaoValida()
+    {
+        $albumartista = new Albunsartistas();
+        $albumartista->idalbum = 7;
+        $albumartista->idartista = 333333; //artista inexistente
+
+        $this->assertFalse($albumartista->validate());
+    }
+
+    public function testRelacaoAlbunsartistasIdArtistaNuloNaoValida()
+    {
+        $albumartista = new Albunsartistas();
+        $albumartista->idalbum = 7;
+        $albumartista->idartista = null; //artista inexistente
+
+        $this->assertFalse($albumartista->validate());
+    }
+
+    public function testRelacaoAlbunsartistasSave()
+    {
+        $albumartista = new Albunsartistas();
+        $albumartista->idalbum = 7;
+        $albumartista->idartista = 3;
+
+        $this->assertTrue($albumartista->save());
+    }
+
+    public function testVerRelacaoAlbumartistaAdicionado()
+    {
+        $this->tester->seeInDatabase(Albunsartistas::tableName(), ['idalbum' => 7, 'idartista' => 3]);
+    }
+
+    public function testAlterarRelacaoAlbunartistaRegistada()
+    {
+
+        $albumartista = Albunsartistas::find()->where(['idalbum' => 7, 'idartista' => 3])->one();
+        $albumartista->idartista = 2;
+        $salvou = $albumartista->save();
+
+        $this->assertTrue($salvou);
+    }
+
+    public function testVerificarRelacaoAlbunsartistasAlterada()
+    {
+        $this->tester->seeInDatabase(Albunsartistas::tableName(), ['idalbum' => 7, 'idartista' => 2]);
+    }
+
+    public function testEliminarRelacaoAlbumartista()
+    {
+        $albumartista = Albunsartistas::find()->where(['idalbum' => 7, 'idartista' => 2])->one();
+        $this->assertIsNumeric($albumartista->delete());
+    }
+
+    public function testVerificarSeRelacaoAlbunartistaFoiApagada()
+    {
+        $this->tester->dontSeeInDatabase(Albunsartistas::tableName(), ['idalbum' => 7, 'idartista' => 2]);
+    }
+
+    /*
+     * Músicas
+     */
+    public function testMusicaIdAlbumInexistenteNaoValida()
+    {
+        $musica = new Musicas();
+        $musica->nome = 'Ouvi dizer';
+        $musica->ficheiro = date("mdyhis") . '.mp3';
+        $musica->idalbuns = 123124354; // album inexistente
+
+        $this->assertFalse($musica->validate());
+    }
+
+    public function testMusicaIdAlbumNuloNaoValida()
+    {
+        $musica = new Musicas();
+        $musica->nome = 'Ouvi dizer';
+        $musica->ficheiro = date("mdyhis") . '.mp3';
+        $musica->idalbuns = null; // album nulo
+
+        $this->assertFalse($musica->validate());
+    }
+
+    public function testMusicaNomeNumeroInteiroNaoValida()
+    {
+        $musica = new Musicas();
+        $musica->nome = 1234; //nome tem de ser string
+        $musica->ficheiro = date("mdyhis") . '.mp3';
+        $musica->idalbuns = 7;
+
+        $this->assertFalse($musica->validate());
+    }
+
+    public function testMusicaNomeNumeroNuloNaoValida()
+    {
+        $musica = new Musicas();
+        $musica->nome = null; //nome tem de ser string
+        $musica->ficheiro = date("mdyhis") . '.mp3';
+        $musica->idalbuns = 7;
+
+        $this->assertFalse($musica->validate());
+    }
+
+    public function testMusicaSave()
+    {
+        $musica = new Musicas();
+        $musica->nome = 'Ouvi dizer'; //nome tem de ser string
+        $musica->ficheiro = date("mdyhis") . '.mp3';
+        $musica->idalbuns = 7;
+
+        $this->assertTrue($musica->save());
+    }
+
+    public function testVerMusicaAdicionada()
+    {
+        $this->tester->seeInDatabase(Musicas::tableName(), ['nome' => 'Ouvi dizer']);
+    }
+
+    public function testAlterarMusicaAdicionada()
+    {
+        $musica = Musicas::find()->where(['nome' => 'Ouvi dizer'])->one();
+        $musica->nome = 'Chaga';
+        $this->assertTrue($musica->save());
+    }
+
+    public function testVerificarMusicaAlterada()
+    {
+        $this->tester->seeInDatabase(Musicas::tableName(), ['nome' => 'Chaga']);
+    }
+
+    public function testEliminarMusica()
+    {
+        $musica = Musicas::find()->where(['nome' => 'Chaga'])->one();
+        $this->assertIsNumeric($musica->delete());
+    }
+
+    public function testVerificarSeMusicaFoiApagada()
+    {
+        $this->tester->dontSeeInDatabase(Musicas::tableName(), ['nome' => 'Chaga']);
+    }
 }
