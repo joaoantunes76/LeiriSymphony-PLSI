d40e7942e966d1ee123c1eae1413a50a37e34053
joaoantunes76 <2190732@my.ipleiria.pt>
2022-01-05 15:54:17 +0000

    Fix footer and navbar, fix produtosIndex (design)

--- leiriSymphony/frontend/views/produtos/index.php
+++ leiriSymphony/frontend/views/produtos/index.php
@@ -30,68 +30,7 @@ $this->title = 'Produtos';
             'subcategorias' => $subcategorias,
         ]) ?>
         <div class="row">
-            <div class="col-lg-3">
-                <div class="left_sidebar_area">
-                    <aside class="left_widgets p_filter_widgets">
-                        <div class="l_w_title">
-                            <h3>Marcas</h3>
-                        </div>
-                        <div class="widgets_inner">
-                            <ul class="list">
-                                <?php
-                                    foreach ($marcas as $marca){
-                                ?>
-                                    <li>
-                                        <a href="#"><?= $marca->nome ?></a>
-                                    </li>
-                                <?php
-                                    }
-                                ?>
-                            </ul>
-                        </div>
-                    </aside>
-
-                    <aside class="left_widgets p_filter_widgets">
-                        <div class="l_w_title">
-                            <h3>Categorias</h3>
-                        </div>
-                        <div class="widgets_inner">
-                            <ul class="list">
-                                <?php
-                                foreach ($categorias as $categoria){
-                                    ?>
-                                    <li>
-                                        <a href="#"><?= $categoria->nome ?></a>
-                                    </li>
-                                    <?php
-                                }
-                                ?>
-                            </ul>
-                        </div>
-                    </aside>
-
-                    <aside class="left_widgets p_filter_widgets">
-                        <div class="l_w_title">
-                            <h3>Subcategorias</h3>
-                        </div>
-                        <div class="widgets_inner">
-                            <ul class="list">
-                                <?php
-                                foreach ($subcategorias as $subcategoria){
-                                    ?>
-                                    <li>
-                                        <a href="#"><?= $subcategoria->nome ?></a>
-                                    </li>
-                                    <?php
-                                }
-                                ?>
-                            </ul>
-                        </div>
-                    </aside>
-                </div>
-            </div>
-
-            <div class="col-lg-9">
+            <div class="col-lg-12">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="product_top_bar d-flex justify-content-between align-items-center">
